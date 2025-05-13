b64095c5822e23f45ce1045a78cd6c373ffade6b
Newfie <157941859+Newfies@users.noreply.github.com>
2025-05-12 21:02:34 -0500

    d

--- script.js
+++ script.js
@@ -13,4 +13,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     // Other Stuff
     loadScript('res/app.js')
+
+    // Progress Bars
+    document.querySelectorAll('.progress-label').forEach(label => {
+        const per = label.getAttribute('per');
+        const originalText = label.textContent.trim();
+        const fillDiv = label.closest('.progress-fill');
+
+        fillDiv.style.width = per + '%';
+        label.textContent = `${originalText} -  ${per}%`;
+    });
 });
