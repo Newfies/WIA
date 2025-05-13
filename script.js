16c414cb8524be1d010a1dcaece19be92705bcf5
Newfie <157941859+Newfies@users.noreply.github.com>
2025-05-12 19:59:11 -0500

    sx

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    // Functions Here
+    function loadScript(url) {
+        const script = document.createElement('script');
+        script.src = url;
+        document.head.appendChild(script);
+    }
+
+    // Event Listeners
+    document.addEventListener('contextmenu', function(event){
+        event.preventDefault();
+    })
+
+    // Other Stuff
+    loadScript('res/app.js')
+});
