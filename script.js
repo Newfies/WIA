bf5f73042da8af04625d1e705dbb2d86b298fb5b
Newfie <157941859+Newfies@users.noreply.github.com>
2025-05-12 21:12:37 -0500

    fw

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+/*
+
+
+
+
+                                                                
+                                                                
+                                                                
+                                                                
+  qq      q                        qqqq                         
+  qqq    qq                       qq  q                         
+  qqqq   q                        q                             
+   q q   q                  q     q      🐶                      
+   q q   q  qqqqq    q      q     q             qqq   qqqqq     
+   q qq  q  q   q    q      q   qq🐶qq         qq q   q   qq    
+   q  q  q  q  🐶q   q  🐶  q     q      q     q  q   qq        
+   q  qq q  qq       q  q   q     qq     q     qq🐶q   qq       
+   qq  qqq   qqqqq   q qqq  q      q     q     q        qq🐶     
+    q  q🐶           qqq qq q      q     q     qq  q      qqq   
+    q                      qq            q      qqqq     qqqq   
+                                                        qq      
+                                                                
+                                                                
+                                                                
+                           q                                    
+                           q    q                               
+                          qq   qq                               
+                          q    q                                
+                         qq   qq                                
+                  q      q    q                                 
+                  q                                             
+                 qq                q                            
+                 q    qq🐶q        q                            
+                 q  qqq  q        qq                            
+                 qqqq    qqqqq   qq                             
+                             qqqqq                              
+
+
+
+
+
+*/
+
 document.addEventListener("DOMContentLoaded", (event) => {
     // Functions Here
     function loadScript(url) {
